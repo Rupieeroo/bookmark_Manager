4e21895671f08e2206f8d5495da73b9e5b9f8bf5
Rupert Adams <RupertNCAdams@aol.com>
2018-10-25 18:10:27 +0100

    chapter 9

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 2. Create a database using the psql command CREATE DATABASE bookmark_manager;
 3. Connect to the database using the psql command \c bookmark_manager;
 4. run the query we have saved in the file 01_create_bookmarks_table.sql
+
+#Setting up test enviroment
+
+1. connect to psql
+2. Create a database using the psql command CREATE DATABASE bookmark_manager_test;
+3. Connect to the database using the psql command \c bookmark_manager_test;
+4. run the query we have saved in the file 01_create_bookmarks_table.sql
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,11 +3,16 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include('www.google.com')
-      expect(bookmarks).to include('www.codeacademy.com')
-      expect(bookmarks).to include('www.facebook.com')
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
     end
 
   end
